4645b30fef7d806a9db5d323237cb578f69a3e12
Iam-Pierre <iampierre01@gmail.com>
2026-03-16 15:06:12 +0100

    create login

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file,session
 from io import BytesIO
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle
+from werkzeug.security import generate_password_hash, check_password_hash
+
+USERS = {}
 
 app = Flask(__name__)
 
@@ -23,11 +26,15 @@ with open('explainer/shap_explainer.pkl', 'rb') as f:
 def index():
     return render_template('template.html', FEATURES=feature_names)
 
+@app.route("/api/login")
+def exercice1():
+    return render_template('login.html')
+
 
 @app.route("/api/predict", methods=['POST'])
 def predict():
     data = request.get_json()
-    features = [float(data[features]) for features in FEATURES]
+    features = [float(data[features]) for features in feature_names]
     prediction = MODEL.predict_proba([features])[0][1]
     return {'prediction': prediction[0][1]}
 
@@ -38,7 +45,18 @@ def predict():
 #     shap_values=EXPLAINER([features])
 
 
+@app.route("/api/login",methods=['POST'])
+def register():
+    data = request.get_json()
+    u = data.get("username","")
+    p = data.get("password","")
 
+    if u in USERS:
+        return{"error": "Nom d'utilisateur déja pris"},400
+    
+    USERS[u] = generate_password_hash(p)
+    session["user"] = u
+    return {"ok": True}
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=4000, debug=True)
